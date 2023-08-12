60a7f028bc1e92a71e29da807d04a3f499a32229
hjb0907 <hjb0907@naver.com>
2023-08-12 21:05:18 +0900

    test한번해봤어요

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,6 +2,7 @@
 
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
   const [hello, setHello] = useState('')
@@ -13,9 +14,12 @@ function App() {
   }, []);
 
   return (
-      <div>
-        컨트롤러에서 가져온 데이터 : {hello}
+    <div>
+      <div class="header">
+        <h1>안녕하세요. 여러분들 블로그에 오신 걸 방가워요.</h1>
       </div>
+      <div>컨트롤러에서 가져온 데이터 : {hello}</div>
+    </div>
   );
 }
 
